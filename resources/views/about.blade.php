6c3199777e54e6c6488e910861358a4db3a03f29
elsyscnl <elizacanilao141@gmail.com>
2024-06-07 09:35:34 +0800

    hehe

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 <div class="card mt-5 shadow-lg">
-                    <div class="card-header bg-orange text-white">
+                    <div class="card-header bg-Pink text-white">
                         <h4 class="mb-0">Employee List</h4>
                     </div>
-                    <div class="card-body bg-white">
+                    <div class="card-body bg-Gray">
                         <table class="table table-bordered table-striped">
                             <thead class="thead-bright">
                               <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
-                                <th scope="col">DOB</th>
-                                <th scope="col">Phone</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Password</th>
+                                <th scope="col">ConfirmPassword</th>
+                                <th scope="col">Email</th>
                                 <th scope="col">Action</th>
                               </tr>
                             </thead>
@@ -21,10 +21,10 @@
                                 @foreach ($employees as $key => $employee)
                                 <tr>
                                     <td>{{ $key + 1 }}</td>
-                                    <td>{{ $employee->first_name }}</td>
-                                    <td>{{ $employee->last_name }}</td>
-                                    <td>{{ $employee->dob }}</td>
-                                    <td>{{ $employee->phone }}</td>
+                                    <td>{{ $employee->Username }}</td>
+                                    <td>{{ $employee->Password }}</td>
+                                    <td>{{ $employee->ConfirmPassword }}</td>
+                                    <td>{{ $employee->Email }}</td>
                                     <td>
                                         <a href="{{ route('employee.edit', $employee->id) }}" class="btn btn-warning btn-sm">
                                             <i class="fa fa-pencil-square-o"></i> Edit
